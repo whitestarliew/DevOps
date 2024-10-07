91bcf0b8c767a29f265716eada1298bfdb5efebb
whitestarliew <whitestarliew@gmail.com>
2024-10-07 20:52:57 +0800

    update

---
+++ docker/src/main/java/com/example/springboot_java_demo/SpringbootJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_java_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaDemoApplication.class, args);
+	}
+
+}
